292ea830ad3f0dc9eed45958a69794aa129fa475
Daryl <Daryl@git.com>
2018-10-30 09:47:08 +0800

    tv

---
+++ Drying/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Drying
+//
+//  Created by Daryl on 2018/10/30.
+//  Copyright © 2018 TG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-Drying/Pods-Drying-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Drying : NSObject
+@end
+@implementation PodsDummy_Pods_Drying
+@end
